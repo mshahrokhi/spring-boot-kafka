617a31abfd3d4200e4780256257e0b363a887735
mohammad shahrokhi <mohammad.shahrokhi72@gmail.com>
2023-11-22 13:49:05 +0330

    Remove an extra class

--- src/main/java/com/shahrokhi/springbootkafka/model/MessageResponse.java
+++ src/main/java/com/shahrokhi/springbootkafka/model/MessageResponse.java
@@ -3,5 +3,12 @@ package com.shahrokhi.springbootkafka.model;
 import lombok.Data;
 
 @Data
-public class MessageResponse extends BaseResponse {
+public class MessageResponse {
+    private String resultCode;
+    private String resultDesc;
+
+    public void setSuccess() {
+        this.resultCode = "200";
+        this.resultDesc = "Success";
+    }
 }
